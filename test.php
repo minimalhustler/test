43727421bf012e0aa89824518c5faeff289b2a31
minimalhustler <90144073+minimalhustler@users.noreply.github.com>
2023-12-28 20:17:45 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:       test
+ * Description:       test plugin
+ * Version:           1
+ * Author:            minimal
+ * Update URI:        https://www.google.seo/pro
+ * Text Domain:       test
+ */
+
+
