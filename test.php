8bf2552d1ff5ddc0fd8db5e8bedb3635aa398e68
minimalhustler <90144073+minimalhustler@users.noreply.github.com>
2023-12-29 10:24:11 +0530

    Update test.php

--- test.php
+++ test.php
@@ -10,3 +10,33 @@
  */
 
 
+
+function check_for_plugin_update() {
+    $current_version = '1.0.0'; // Replace with your current version
+    $api_url = 'https://api.github.com/repos/yourusername/test/releases/latest';
+
+    // Make a remote request to your update-check endpoint
+    $response = wp_remote_get($api_url);
+
+    if (is_array($response)) {
+        $body = $response['body'];
+        $data = json_decode($body);
+
+        if ($data && version_compare($data->tag_name, $current_version, '>')) {
+            // New version available, notify the user
+            add_action('admin_notices', 'display_update_notification');
+        }
+    }
+}
+
+function display_update_notification() {
+    ?>
+    <div class="notice notice-info is-dismissible">
+        <p>Test Plugin has a new version available! <a href="<?php echo esc_url($data->zipball_url); ?>">Update now</a>.</p>
+    </div>
+    <?php
+}
+
+add_action('admin_init', 'check_for_plugin_update');
+
+
